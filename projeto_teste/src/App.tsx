849c832efd6d66aa7574edca631c8d3f989c25c9
Natasha Angi <96844887+NatashaAngi@users.noreply.github.com>
2025-08-17 13:15:54 -0300

    Teste

---
+++ projeto_teste/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Profile from './components/Profile'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Teste app</h1>
+     <h2>Teste 2</h2>
+     <Profile />
+    </>
+  )
+}
+
+export default App
---
+++ projeto_teste/src/components/Profile.tsx
@@ -0,0 +1,8 @@
+export default function Profile (){
+    return (
+        <>
+
+        <h1>TESTE DE COMPONENTE </h1>
+        </>
+    )
+}
